fed41c4659a704a4cac756a146efa3099cf53055
denis-fominykh <fominykhdenis@gmail.com>
2020-07-30 18:28:01 +0300

    adding a table component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React, { FC } from 'react';
 
 import './App.scss';
 
+import Table from './components/Table';
+
 const App: FC = () => {
   return (
     <div className="app">
-      <h1>Hello, Yalantis!</h1>
+      <Table />
     </div>
   );
 };
