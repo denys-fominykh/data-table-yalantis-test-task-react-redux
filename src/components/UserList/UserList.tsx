8faf554b85f9c3d1066363662d19c52754e829ab
denis-fominykh <fominykhdenis@gmail.com>
2020-08-02 13:21:08 +0300

    creating a userlist component

---
+++ src/components/UserList/UserList.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+
+import { UserType } from '../../types/TUser';
+import User from '../User';
+
+interface UserListProps {
+  users: UserType[];
+}
+
+const UserList: FC<UserListProps> = ({ users }) => {
+  return (
+    <table className="table table-striped table-sm">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Date of Birthday</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, idx) => {
+          return <User key={idx} user={user} idx={idx} />;
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserList;
